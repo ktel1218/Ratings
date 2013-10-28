0e11f296709d9e6c1318889e87a66a2f0bc0fd25
Hackbright Student <user@chromebox-003.(none)>
2013-10-28 16:40:57 -0700

    Initial commit with seeded database

--- model.py
+++ model.py
@@ -1,17 +1,68 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, DateTime, Date
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
 
+ENGINE = None
+Session = None
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__= "users"
+
+    id = Column(Integer, primary_key= True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+
+class Movie(Base):
+    __tablename__="movies"
+
+    id = Column(Integer, primary_key = True)
+    title = Column(String(100))
+    release_date = Column(Date)
+    url = Column(String(2083))
+
+class Rating(Base):
+    __tablename__="ratings"
+
+    id= Column(Integer, primary_key= True)
+    user_id = Column(Integer)
+    movie_id = Column(Integer)
+    rating = Column(Integer)
+    time_stamp = Column(DateTime)
+
+    # ratings = relationship("Movie", back_ref = "id")
 
 
 ### End class declarations
 
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
+    Session = sessionmaker(bind=ENGINE)
+    return Session()
+
 def main():
-    """In case we need this for something"""
-    pass
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
+    Session = sessionmaker(bind=ENGINE)
+
+
+def initialize_tables():    
+    """Initialize tables"""
+    global ENGINE
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Base.metadata.create_all(ENGINE)
 
 if __name__ == "__main__":
     main()
--- seed.py
+++ seed.py
@@ -1,22 +1,69 @@
 import model
 import csv
+from datetime import datetime
 
 def load_users(session):
     # use u.user
-    pass
+    f = open("seed_data/u.user")
+    entries = f.read().split("\n")
+    f.close()
+    for item in entries:
+        item = item.split("|")
+        user = model.User(id = int(item[0]), age = int(item[1]), zipcode = item[4])
+        session.add(user)
+
+    session.commit()
 
 def load_movies(session):
-    # use u.item
-    pass
+    f = open("seed_data/u.item")
+    entries = f.read().split("\n")
+    f.close()
+    for item in entries:
+
+        item = item.split("|")
+        title = item[1]
+        date = item[2]
+        if title != "unknown" and date != None:
+            date_formatted = datetime.strptime(date, "%d-%b-%Y")
+            title_formatted =  title.decode("latin-1")
+            movie = model.Movie(id = int(item[0]), title = title_formatted, release_date = date_formatted, url = item[4])
+            session.add(movie)
+    
+    session.commit()
+
 
 def load_ratings(session):
     # use u.data
-    pass
+    f = open("seed_data/u.data")
+    entries = f.read().split("\n")
+    f.close()
+    for item in entries:
+        item = item.split()
+        timestamp = int(item[3])
+        formatted_time = datetime.utcfromtimestamp(timestamp)
+        rating = model.Rating(user_id = int(item[0]), movie_id = int(item[1]), rating = int(item[2]), time_stamp = formatted_time)
+        session.add(rating)
+
+    session.commit()
+
+
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    load_movies(session)
+    load_ratings(session)
 
 if __name__ == "__main__":
     s= model.connect()
     main(s)
+
+
+
+
+# open a file
+# read a line
+# parse a line
+# create an object
+# add the object to a session
+# commit
+# repeat until done
