310ac518b38161dea78bd212e7b921cfebe3cc99
Hackbright Student <user@chromebox-003.(none)>
2013-10-28 17:38:27 -0700

    Created intial flask framework

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 from sqlalchemy import Column, Integer, String, DateTime, Date
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship, backref
 
-ENGINE = None
-Session = None
+
+
+engine = create_engine("sqlite:///ratings.db", echo = False)
+session = scoped_session(sessionmaker(bind=engine, autocommit = False, autoflush = False))
 Base = declarative_base()
+Base.query = session.query_property()
 
 ### Class declarations go here
 class User(Base):
@@ -32,31 +33,20 @@ class Rating(Base):
     __tablename__="ratings"
 
     id= Column(Integer, primary_key= True)
-    user_id = Column(Integer)
-    movie_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    movie_id = Column(Integer, ForeignKey('movies.id'))
     rating = Column(Integer)
     time_stamp = Column(DateTime)
 
-    # ratings = relationship("Movie", back_ref = "id")
+    user = relationship("User", backref=backref("ratings", order_by=id))
+    movie = relationship("Movie", backref=backref("ratings", order_by=id))
 
 
 ### End class declarations
-
-def connect():
-    global ENGINE
-    global Session
-
-    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
-    Session = sessionmaker(bind=ENGINE)
-    return Session()
+    
 
 def main():
-    global ENGINE
-    global Session
-
-    ENGINE = create_engine("sqlite:///ratings.db", echo = True)
-    Session = sessionmaker(bind=ENGINE)
-
+    """nothing doing"""
 
 def initialize_tables():    
     """Initialize tables"""
